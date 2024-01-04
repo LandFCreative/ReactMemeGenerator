d7d2352848d64cbea084bf78637c2c8b8c3f87f7
LandFCreative <deneschukc@yahoo.ca>
2024-01-03 17:57:39 -0700

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Footer() {
+return (
+    <footer className="footer">
+    <div>
+        Coded by{" "}
+        <a className="footerlink"
+        href="https://genuine-licorice-99a32a.netlify.app/" target="_blank" >
+        Christine at Lost and Found Creative
+        </a>
+    </div>
+    </footer>
+);
+}
